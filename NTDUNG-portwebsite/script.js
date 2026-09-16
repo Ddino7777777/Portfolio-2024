7fde94b81333255fea106472a47894befe8da069
Ddino7777777 <fangirl1412005@gmail.com>
2026-09-16 22:15:17 +0700

    Add files via upload

---
+++ NTDUNG-portwebsite/script.js
@@ -0,0 +1,20 @@
+// pt.2 effect
+(function() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const textElements = gsap.utils.toArray('.text');
+
+  textElements.forEach(text => {
+    gsap.to(text, {
+      backgroundSize: '100%',
+      ease: 'none',
+      scrollTrigger: {
+        trigger: text,
+        start: 'center 80%',
+        end: 'center 20%',
+        scrub: true,
+      },
+    });
+  });
+  })
+// pt.3 content
